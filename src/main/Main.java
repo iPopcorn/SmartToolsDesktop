b04ade3e5c7bfda921dd0391ab52fd3e2103c670
Maurice Harris Jr <mwharrisjr@gmail.com>
2016-12-30 12:06:04 -0600

    Foundation for Desktop Application.

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,7 +14,6 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        System.out.println("Hello, World!");
         // set stage
         myStage = primaryStage;
 
