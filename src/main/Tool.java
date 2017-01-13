098a7074c3433ddbb0a21e87721d0ac0fc537745
Maurice Harris Jr <mwharrisjr@gmail.com>
2017-01-13 14:27:40 -0600

    Created 'Tool', 'ToolCell', and 'ToolCellFactory', and 'LookupController' classes in order to implement the preliminary look up tool functionality.

---
+++ src/main/Tool.java
@@ -0,0 +1,50 @@
+package main;
+
+/**
+ * Created by mwhar on 1/10/2017.
+ */
+public class Tool {
+
+    private String name;
+    private String id;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Tool(String name, String id, String address) {
+        this.name = name;
+        this.id = id;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Tool{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
