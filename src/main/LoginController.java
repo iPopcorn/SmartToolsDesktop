616af790ca93d7445103153c1796805d5456d40c
iPopcorn <taypetrillo@gmail.com>
2017-02-10 14:06:26 -0600

    Connected authentication to database.

--- src/main/LoginController.java
+++ src/main/LoginController.java
@@ -9,6 +9,7 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 /**
  * Created by Taylor on 2/9/2017.
@@ -28,12 +29,23 @@ public class LoginController {
         System.out.printf("Username: %s\nPassword: %s\nHashed Password: %s\n", username, password, hashedPW);
 
         //todo: add authentication logic
-        try{
-            openAdmin(actionEvent);
-        }catch(IOException e){
-            System.out.println(e);
-        }
+        ServerRequest request = new ServerRequest();
+        HashMap<String, String> POSTdata = new HashMap<>();
+        POSTdata.put("username", username);
+        POSTdata.put("password", hashedPW);
 
+        String response = request.getResponseFromRequest("login/login.php", POSTdata);
+
+        if(response.equalsIgnoreCase("success")){
+            try{
+                openAdmin(actionEvent);
+            }catch(IOException e){
+                System.out.println(e);
+            }
+        }else{
+            //todo: insert GUI error message
+            System.out.println("Invalid password.");
+        }
     }
 
     public void goBack(ActionEvent actionEvent) throws IOException {
