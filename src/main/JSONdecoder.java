1bac9d143bb833aa0f00d33d7e814626fbf93491
Gabino Luna <gabinoln@yahoo.com>
2017-04-05 15:01:17 -0500

    New JSON decoding function for the tool list in the DB.

--- src/main/JSONdecoder.java
+++ src/main/JSONdecoder.java
@@ -62,4 +62,34 @@ public class JSONdecoder {
         }
         return openAddresses;
     }
+
+    /*
+    decodeJSONAddressResponse(String JSONstring)
+    PARAMETERS: JSONString, a string in the JSON format in the form of
+    "{"tool_name":"zzzzzz"}"
+    RETURN VALUES: Returns an arraylist of class Tool with each tool read-in from the JSON response.
+  */
+    public ArrayList<Tool> decodeJSONToolListResponse(String JSONstring) {
+        String[] jsonStrings = JSONstring.split("(?<=})"); // reading
+        ArrayList<Tool> toolList = new ArrayList<>(); // the list of tools to be returned
+        Tool tempTool; // temporary tool object to be added to tool list
+
+        JSONObject tempJSON; // temp JSON object from each tool read in
+        try {
+            for (String string : jsonStrings) {
+                tempJSON = new JSONObject(string);
+                tempTool = new Tool(tempJSON.getString("tool_name"), "0",
+                        "0");
+                toolList.add(tempTool);
+            }
+            for (Tool tool : toolList) {
+                System.out.println(tool.toString());
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            System.out.println("ERROR READING THE JSON");
+        }
+        return toolList;
+    }
 }
