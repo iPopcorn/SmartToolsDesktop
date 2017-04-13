eaadab8079106f858d3eb81e02a36b907a9d87e7
iPopcorn <taypetrillo@gmail.com>
2017-04-13 17:02:49 -0500

    Modified Classes: LoginController.java     - Added documentation     - Added a popup error message Main.java     - Added documentation NameComparator.java     - Added documentation

--- src/main/LoginController.java
+++ src/main/LoginController.java
@@ -12,15 +12,31 @@ import java.io.IOException;
 import java.util.HashMap;
 
 /**
- * Created by Taylor on 2/9/2017.
+ * LoginController - Controller for the login.fxml view, implements the functionality of the view
  */
 public class LoginController {
 
+    /** Text field that holds the username*/
     public TextField txtUsername;
+
+    /** Text field that holds the password*/
     public TextField txtPassword;
+
+    /** Button that returns the user back to the main menu*/
     public Button btnBack;
+
+    /** Button that submits login credentials to the system*/
     public Button btnLogIn;
 
+    /** Grabs the text from the GUI and sends it to the database for authentication.
+     *
+     * void submitLogin(ActionEvent) - submitLogin pulls the text from the GUI. It hashes the password text, and then
+     * packages the username and hashed password into a HashMap that is sent to the database. The response will be
+     * parsed, and if the authentication is successful, the ActionEvent will be passed to the openAdmin() method.
+     * Otherwise, an error message will pop up.
+     *
+     * @param actionEvent The ActionEvent that triggered this callback.
+     * */
     public void submitLogin(ActionEvent actionEvent) {
         String username, password, hashedPW;
         username = txtUsername.getText();
@@ -28,7 +44,6 @@ public class LoginController {
         hashedPW = org.apache.commons.codec.digest.DigestUtils.sha256Hex(password);
         System.out.printf("Username: %s\nPassword: %s\nHashed Password: %s\n", username, password, hashedPW);
 
-        //todo: add authentication logic
         ServerRequest request = new ServerRequest();
         HashMap<String, String> POSTdata = new HashMap<>();
         POSTdata.put("username", username);
@@ -45,9 +60,18 @@ public class LoginController {
         }else{
             //todo: insert GUI error message
             System.out.println("Invalid password.");
+            PopupWindow error = new PopupWindow("Error", "Invalid Password!");
+            error.popup();
         }
     }
 
+    /** Opens the Admin screen
+     *
+     *  void openAdmin(ActionEvent) - openAdmin() displays the Admin screen in the GUI. It is called by submitLogin()
+     *  upon a successful authentication attempt.
+     *
+     *  @param actionEvent The ActionEvent passed in by submitLogin()
+     * */
     public void openAdmin(ActionEvent actionEvent) throws IOException{
         Stage stage = null;
         Parent root = null;
