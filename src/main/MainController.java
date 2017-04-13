9a06787ef855a4925008c32ec1961d07ee37e45e
Gabino Luna <gabinoln@yahoo.com>
2017-04-12 19:58:59 -0500

    Added sign in window - still need logic.

--- src/main/MainController.java
+++ src/main/MainController.java
@@ -1,8 +1,12 @@
 package main;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -10,6 +14,9 @@ import java.io.IOException;
  * Created by mwhar on 3/25/2017.
  */
 public class MainController {
+
+    private boolean signedIn = false; // boolean to check if the user is signed in
+
     /** Tab to switch to the Search Inventory screen. */
     public Tab searchTools;
 
@@ -24,6 +31,9 @@ public class MainController {
 
     /** Tab to switch to the Report Inventory screen. */
     public Tab reportInventory;
+
+    /** Tab to popup the sign in screen. */
+    public Tab tabSignIn;
 
     /** BorderPane where the main elements of the application will be displayed. */
     public BorderPane borderPane;
@@ -43,8 +53,20 @@ public class MainController {
             borderPane.setCenter(FXMLLoader.load(getClass().getResource("res/fxml/delete_tool.fxml")));
         else if(modifyInventory.isSelected())
             borderPane.setCenter(FXMLLoader.load(getClass().getResource("res/fxml/modify_inventory.fxml")));
-        else
+        else if(reportInventory.isSelected())
             borderPane.setCenter(FXMLLoader.load(getClass().getResource("res/fxml/generate_report.fxml")));
+        else if (tabSignIn.isSelected()) {
+            Parent root;
+            root = FXMLLoader.load(getClass().getResource("res/fxml/sign_in_window.fxml"));
+            Stage signInWindow = new Stage();
+            signInWindow.initModality(Modality.APPLICATION_MODAL);
+            Scene scene = new Scene(root, 400, 200);
+
+            signInWindow.setScene(scene);
+            signInWindow.setResizable(false);
+            signInWindow.show();
+            System.out.printf("here");
+        }
 
     }
 }
---
+++ src/main/SignInController.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * Created by gabinoluna on 4/12/17.
+ */
+public class SignInController {
+
+    public void signInPressed() {
+        System.out.printf("HEre!");
+    }
+}
