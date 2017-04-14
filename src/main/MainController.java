84704f70f1b4633dcd9345b368af31e0a842921e
Gabino Luna <gabinoln@yahoo.com>
2017-04-13 23:43:43 -0500

    Sign in value sent to main controller, need to add logic to change screens

--- src/main/MainController.java
+++ src/main/MainController.java
@@ -17,56 +17,110 @@ public class MainController {
 
     private boolean signedIn = false; // boolean to check if the user is signed in
 
-    /** Tab to switch to the Search Inventory screen. */
+    /**
+     * Tab to switch to the Search Inventory screen.
+     */
     public Tab searchTools;
 
-    /** Tab to switch to the Replace Tool screen. */
+    /**
+     * Tab to switch to the Replace Tool screen.
+     */
     public Tab replaceTool;
 
-    /** Tab to switch to the Replace Tool screen. */
+    /**
+     * Tab to switch to the Replace Tool screen.
+     */
     public Tab deleteTool;
 
-    /** Tab to switch to the Modify Inventory screen. */
+    /**
+     * Tab to switch to the Modify Inventory screen.
+     */
     public Tab modifyInventory;
 
-    /** Tab to switch to the Report Inventory screen. */
+    /**
+     * Tab to switch to the Report Inventory screen.
+     */
     public Tab reportInventory;
 
-    /** Tab to popup the sign in screen. */
+    /**
+     * Tab to popup the sign in screen.
+     */
     public Tab tabSignIn;
 
-    /** BorderPane where the main elements of the application will be displayed. */
+    /**
+     * BorderPane where the main elements of the application will be displayed.
+     */
     public BorderPane borderPane;
 
-    /** Changes the screen shown to the user based on the tab selected.
+    /**
+     * Changes the screen shown to the user based on the tab selected.
+     * <p>
+     * void changeScreen() - Changes the screen and tool bar buttons shown to the user based on the tab selected.
      *
-     *  void changeScreen() - Changes the screen and tool bar buttons shown to the user based on the tab selected.
-     *
-     *  @throws IOException
+     * @throws IOException
      */
     public void changeScreen() throws IOException {
-        if(searchTools.isSelected())
+        if (searchTools.isSelected())
             borderPane.setCenter(FXMLLoader.load(getClass().getResource("res/fxml/lookup_tool.fxml")));
-        else if(replaceTool.isSelected())
+        else if (replaceTool.isSelected())
             borderPane.setCenter(FXMLLoader.load(getClass().getResource("res/fxml/add_tool.fxml")));
-        else if(deleteTool.isSelected())
+        else if (deleteTool.isSelected())
             borderPane.setCenter(FXMLLoader.load(getClass().getResource("res/fxml/delete_tool.fxml")));
-        else if(modifyInventory.isSelected())
+        else if (modifyInventory.isSelected())
             borderPane.setCenter(FXMLLoader.load(getClass().getResource("res/fxml/modify_inventory.fxml")));
-        else if(reportInventory.isSelected())
+        else if (reportInventory.isSelected())
             borderPane.setCenter(FXMLLoader.load(getClass().getResource("res/fxml/generate_report.fxml")));
         else if (tabSignIn.isSelected()) {
-            Parent root;
-            root = FXMLLoader.load(getClass().getResource("res/fxml/sign_in_window.fxml"));
-            Stage signInWindow = new Stage();
-            signInWindow.initModality(Modality.APPLICATION_MODAL);
-            Scene scene = new Scene(root, 400, 200);
+            if (signedIn == false) {
 
-            signInWindow.setScene(scene);
-            signInWindow.setResizable(false);
-            signInWindow.show();
-            System.out.printf("here");
+                Parent root;
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("res/fxml/sign_in_window.fxml"));
+                root = fxmlLoader.load();
+                SignInController signInController = fxmlLoader.getController();
+
+
+                Stage signInWindow = new Stage();
+                signInWindow.initModality(Modality.APPLICATION_MODAL);
+                Scene scene = new Scene(root, 400, 200);
+
+                signInWindow.setScene(scene);
+                signInWindow.setResizable(false);
+                signInWindow.showAndWait();
+
+                signedIn = signInController.btnSignInPressed();
+
+                System.out.println(signedIn);
+            } else {
+                setSignedIn(false);
+
+            }
         }
+    }
 
+    public void setSignedIn(boolean signedIn) {
+
+        // if we're switching signed in states
+        if (this.signedIn != signedIn) {
+            this.signedIn = signedIn;
+            if (signedIn) {
+                updateSignedInState(true);
+            } else {
+                updateSignedInState(false);
+            }
+
+        } else { // we are not switching signed in states so nothing changes
+
+        }
+    }
+    public void updateSignedInState(boolean signedIn) {
+        if (signedIn) {
+
+        } else {
+
+        }
+    }
+
+    public boolean isSignedIn() {
+        return this.signedIn;
     }
 }
--- src/main/SignInController.java
+++ src/main/SignInController.java
@@ -1,11 +1,81 @@
 package main;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.util.HashMap;
+
 /**
- * Created by gabinoluna on 4/12/17.
+ * The SignInController will handle the operation of signing in and authorizing the user
+ * in order to give full access to the program and update the Main Controller which handles
+ * the main view of the program.
  */
 public class SignInController {
+    @FXML
+    private TextField tfUserName, tfPassword;
+    @FXML
+    private Label lblUNEmpty, lblPWEmpty;
+    @FXML
+    private ImageView imgUNEmpty, imgPWEmpty;
+    @FXML
+    private Button btnCancel;
 
-    public void signInPressed() {
-        System.out.printf("HEre!");
+    // if the sign in button is pressed the request will be proccessed
+    public boolean btnSignInPressed() {
+        String username = tfUserName.getText();
+        String password = tfPassword.getText();
+        imgPWEmpty.setVisible(false);
+        lblPWEmpty.setVisible(false);
+        imgUNEmpty.setVisible(false);
+        lblUNEmpty.setVisible(false);
+
+        // if either the password or username is empty we alert the user
+        if (password.isEmpty() || username.isEmpty()) {
+            if (password.isEmpty() && username.isEmpty()) {
+                imgPWEmpty.setVisible(true);
+                lblPWEmpty.setVisible(true);
+                imgUNEmpty.setVisible(true);
+                lblUNEmpty.setVisible(true);
+            } else if (password.isEmpty()) {
+                imgPWEmpty.setVisible(true);
+                lblPWEmpty.setVisible(true);
+            } else if (username.isEmpty()) {
+                imgUNEmpty.setVisible(true);
+                lblUNEmpty.setVisible(true);
+            }
+
+            return false;
+        }
+
+        ServerRequest request = new ServerRequest();
+        HashMap<String, String> POSTdata = new HashMap<>();
+        String hashedPW = org.apache.commons.codec.digest.DigestUtils.sha256Hex(password);
+
+        POSTdata.put("username", username);
+        POSTdata.put("password", hashedPW);
+
+        String response = request.getResponseFromRequest("login/login.php", POSTdata);
+
+        if (response.equalsIgnoreCase("success")) {
+            System.out.println("logged in :)");
+            Stage myStage = (Stage) btnCancel.getScene().getWindow();
+            myStage.close();
+            return true;
+        } else {
+            System.out.println("not logged in :(");
+            System.out.println("bad credentials!");
+        }
+
+        return false;
+    }
+
+    // if the cancel button is pressed the window will be closed
+    public void btnCancelPressed() {
+        Stage myStage = (Stage) btnCancel.getScene().getWindow();
+        myStage.close();
     }
 }
